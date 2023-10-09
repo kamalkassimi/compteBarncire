96fd86b62f4f3433e536dcaece139458beb2a656
kamalkassimi <othmankassimi07@gmail.com>
2023-10-09 10:41:43 +0100

    modifition code sours progmme test

--- test.java
+++ test.java
@@ -10,19 +10,34 @@ import java.util.Stack;
 public class test {
     public static void main(String[] args) {
 
-        CompteCourant compte1 = new CompteCourant("CC", 3000,StatutCompte.SUPPRIME, 100);
-        CompteCourant compte2 = new CompteCourant("CC11", 3000,StatutCompte.SUPPRIME, 100);
+        try{
+            CompteEpargne compte2 = new CompteEpargne("CE001", 5000.0, StatutCompte.SUSPENDU, 0.05);
+            CompteCourant compte1 = new CompteCourant("C03", 5000.0, StatutCompte.BLOQUE, 0.05);
+          //  compte2.virement(100);
+            //compte2.retrait(300);
+            compte2.transfererVers(compte1,100);
+            //System.out.println( compte2.Operations());
+            System.out.println(compte1);
+            System.out.println("compte2");
+            System.out.println(compte2);
+            System.out.println( compte2.consulterTotalretrait());
+            System.out.println(compte2.consulterTotalVersements());
 
-        //CompteEpargne compte2 = new CompteEpargne("null", 1000,StatutCompte.CREE, 0.05);
-        // compte1.transfererVers(compte2,300);
-         compte1.retrait(100);
-        // compte1.virement(400);
-        // compte1.retrait(100);
-        // compte1.retrait(100);
-        System.out.println(compte1);
-       System.out.println( compte2);
-       System.out.println(compte1.consulterTotalretrait());
-       System.out.println(compte1.consulterTotalVersements());
+//             List<Operation> listeComptes = new ArrayList<>();
+//             listeComptes.add(operation1);
+            FileWriter file= new FileWriter("C:\\Users\\DELL\\IdeaProjects\\test\\src\\compteBancire\\test.txt");
+            file.write(String.valueOf(compte2.Operations()));
+            file.write(String.valueOf("\nconsulterTotalVersements"+compte2.consulterTotalVersements()));
+            file.write(String.valueOf("\nconsulterTotalretrait"+compte2.consulterTotalretrait()));
+             file.close();
+             file.flush();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }finally {
+            System.out.println("fin programme thank you ...");
+        }
+
+
     }}
 
 
